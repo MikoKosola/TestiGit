99b62bca2d3e943ff6c969e010013a89f0af6131
Joona <joona-rahm@hotmail.com>
2025-05-13 13:56:33 +0300

    en tiiä

--- testi.py
+++ testi.py
@@ -3,9 +3,6 @@ print("Testi lause 2")
 print("Testi lause 3")
 
 # Tämä on kommentti jonka lisäsin feature-joona branchiin
-<<<<<<< HEAD
-muuttuja = 69
-=======
+
 muuttuja = 69
 asdasd = "muuttja"
->>>>>>> feature-joona
